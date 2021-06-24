61b64276e9ae6ed05173325b02bbd1762d0d3300
Adrian-Gonzalez-Revature <adrian.gonzalez@revature.net>
2021-06-23 20:52:14 -0600

    Finished Creating ModelLayer, BusinessLayer, and TestLayer

---
+++ MyEcommerceWebsite/ModelLayer/AddressModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    /// <summary>
+    /// This Model represents an address
+    /// </summary>
+    public class AddressModel
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        public AddressModel() { }
+
+        /// <summary>
+        /// This constructor is reponsible for initialzing the AddressModel object.
+        /// </summary>
+        /// <param name="Street"></param>
+        /// <param name="City"></param>
+        /// <param name="State"></param>
+        /// <param name="Zipcode"></param>
+        public AddressModel(string Street, string City, string State, string Zipcode)
+        {
+            this.Street = Street;
+            this.City = City;
+            this.State = State;
+            this.ZipCode = Zipcode;
+        }
+    }
+}
---
+++ MyEcommerceWebsite/ModelLayer/CategoryModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public class CategoryModel
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+
+        public CategoryModel() { }
+
+        /// <summary>
+        /// This Constructor is reposible for initializing the state of the object
+        /// </summary>
+        /// <param name="CategoryId"></param>
+        /// <param name="CategoryName"></param>
+        public CategoryModel(int CategoryId, string CategoryName)
+        {
+            this.CategoryId = CategoryId;
+            this.CategoryName = CategoryName;
+        }
+
+        /// <summary>
+        /// This Constructor is mainley used for whenever the product is being initialized
+        /// </summary>
+        /// <param name="categoryObj"></param>
+        public CategoryModel(CategoryModel categoryObj)
+        {
+            this.CategoryId = categoryObj.CategoryId;
+            this.CategoryName = categoryObj.CategoryName;
+        }
+    }
+}
